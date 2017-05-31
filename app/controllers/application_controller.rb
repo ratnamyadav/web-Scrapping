b176b2f68dfe7766951eb5ef4c21e9c85be67627
Ratnam Yadav <ratnam.yadav@icloud.com>
2017-06-01 02:08:36 +0530

    login check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,22 +11,26 @@ class ApplicationController < ActionController::Base
     page.form.submit
 
     page = agent.get('https://curegistry.flhealth.gov/Patient/EditPatient/0')
-    page.form['LastName'] = params[:LastName]
-    page.form['FirstName'] = params[:FirstName]
-    page.form['MiddleInitial'] = params[:MiddleInitial]
-    page.form['PrimaryPhone'] = params[:PrimaryPhone]
-    page.form['EmailAddress'] =  params[:EmailAddress]
-    page.form['DateOfBirth'] =  params[:DateOfBirth]
-    page.form['SSN'] =  params[:SSN]
-    page.form['PatientWeight'] =  params[:PatientWeight]
-    page.form['WeightType'] =  params[:WeightType]
-    page.form['Address1'] =  params[:Address1]
-    page.form['Address2'] =  params[:Address2]
-    page.form['City'] =  params[:City]
-    page.form['Zipcode'] =  params[:Zipcode]
-    page.form['GenderID'] =  params[:GenderID]
-    page.form['County'] =  params[:County]
-    page.form.submit
-    render json: {successfull: true}
+    if page.uri.to_s != 'https://curegistry.flhealth.gov/Patient/EditPatient/0'
+      render json: {success: false, message: 'Login Error'}
+    else
+      page.form['LastName'] = params[:LastName]
+      page.form['FirstName'] = params[:FirstName]
+      page.form['MiddleInitial'] = params[:MiddleInitial]
+      page.form['PrimaryPhone'] = params[:PrimaryPhone]
+      page.form['EmailAddress'] =  params[:EmailAddress]
+      page.form['DateOfBirth'] =  params[:DateOfBirth]
+      page.form['SSN'] =  params[:SSN]
+      page.form['PatientWeight'] =  params[:PatientWeight]
+      page.form['WeightType'] =  params[:WeightType]
+      page.form['Address1'] =  params[:Address1]
+      page.form['Address2'] =  params[:Address2]
+      page.form['City'] =  params[:City]
+      page.form['Zipcode'] =  params[:Zipcode]
+      page.form['GenderID'] =  params[:GenderID]
+      page.form['County'] =  params[:County]
+      page.form.submit
+      render json: {success: true, message: 'Patient successfully created'}
+    end
   end
 end
